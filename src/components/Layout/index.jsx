8e026d535c662e8255e7f0b9d4fc0aee53ff721d
mnin-nahid <nahidulislam.nayan54@gmail.com>
2022-08-02 02:40:50 +0600

    almost finised sidebar

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,4 +1,5 @@
 import Sidebar from '../Sidebar';
+import { Outlet  } from "react-router-dom";
 import './index.scss';
 
 const Layout = () => {
@@ -7,6 +8,14 @@ const Layout = () => {
             <Sidebar />
             <div className='page'>
                 <span className='tags top-tags'>&lt;body&gt;</span>
+
+                <Outlet />
+
+                <span className='tags bottom-tags'>
+                &lt;/body&gt;
+                <br />
+                <span className='bottom-tag-html'>&lt;/html&gt;</span>
+                </span>
             </div>
         </div>
     )
